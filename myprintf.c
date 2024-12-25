b99b0fd64441b4a7a10f1db7dc6d47a9c9b88702
veredg1 <vgottlie@mail.yu.edu>
2024-12-25 16:55:14 -0500

    realizing that figuring out the modifiers needed some change. stuck still on how to get the things to print out properly once modified

--- myprintf.c
+++ myprintf.c
@@ -10,26 +10,35 @@ void myprintf(const char * format, ... ) {
     va_list args;
     va_start(args,format);
 
+    _Bool wasPercent = 0; // start looking for formatters if a % is found.
+    _Bool wasFormat = 0; //figure out if the last thing was a formatter
+    _Bool isBackSlash = 0; // if it's a backslash that might need to work properly to modify certain things
+    
 
     while (*format != '\0') {
 
-        if (*format == 'd') {
+        printf("%c\n",*format);
+
+
+        if (*format == '%' && !(wasPercent)) { //first sign of a modifier
+            wasPercent = 1;
+        }
+        if (*format == 'd' && wasPercent) { // then check for later modifiers
             int intArg = va_arg(args,int);
-            putc_unlocked((int)intArg,stdout);
-
-
-
-
-           
+            putchar(intArg);
+            wasFormat = 1;
+            wasPercent = 0;
 
         }
-        else if (*format == 'c') {
+        else if (*format == 'c' && wasPercent) { // then check for existing modifiers
             int charArg = va_arg(args,int);
             putchar(charArg);
-
+            wasFormat = 1;
+            wasPercent = 0;
 
 
         }
+        // check modifiers and if the last thing was a format.
         ++format;
 
     }
